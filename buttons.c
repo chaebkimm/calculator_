2bbc46834a0328f8294720fc2f1441df06bc4350
chaebkimm <chae.b.kim@gmail.com>
2025-02-19 20:35:06 +0900

    ver0

---
+++ buttons.c
@@ -0,0 +1,43 @@
+#include "buttons.h"
+
+extern int result;
+
+void button0() { // 0번 버튼을 누르면 실행되는 함수
+
+}
+
+void button1() { // 1번 버튼을 누르면 실행되는 함수
+
+}
+
+void button2() { // 2번 버튼을 누르면 실행되는 함수
+
+}
+
+void button3() { // 3번 버튼을 누르면 실행되는 함수
+
+}
+
+void button4() { // 4번 버튼을 누르면 실행되는 함수
+
+}
+
+void button5() { // 5번 버튼을 누르면 실행되는 함수
+
+}
+
+void button6() { // 6번 버튼을 누르면 실행되는 함수
+
+}
+
+void button7() { // 7번 버튼을 누르면 실행되는 함수
+
+}
+
+void button8() { // 8번 버튼을 누르면 실행되는 함수
+
+}
+
+void button9() { // 9번 버튼을 누르면 실행되는 함수
+
+}
